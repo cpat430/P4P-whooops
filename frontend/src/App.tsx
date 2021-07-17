e0dd145e867c30b96134a15d2b7e4600e0c9ead8
cpat430 <cpat430@aucklanduni.ac.nz>
2021-07-17 20:20:07 +1200

    added eslint to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import MapPage from './pages/MapPage';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <MapPage />
