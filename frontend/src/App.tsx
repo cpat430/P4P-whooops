65f6faac10773a334b5b8fc8547c10dfe9d6b2c8
cpat430 <cpat430@aucklanduni.ac.nz>
2021-07-17 12:21:40 +1200

    added man page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MapPage from './pages/MapPage';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <MapPage />
     </div>
   );
 }
