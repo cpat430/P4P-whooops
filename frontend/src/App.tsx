c35057970019bc83baf9046fd7dc0b264df2c70b
Shih-Hao Chen <samuelshchen@gmail.com>
2021-07-20 11:16:05 +1200

    created userprovider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import UserContext from './contexts/UserContext';
+import { UserProvider } from './contexts/UserContext';
 import MapPage from './pages/MapPage';
 
 function App(): JSX.Element {
   return (
-    <UserContext.Provider value={{ name: 'Provider default', interests: [] }}>
+    <UserProvider>
       <div className="App">
         <MapPage />
       </div>
-    </UserContext.Provider>
+    </UserProvider>
   );
 }
 
--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -1,9 +1,18 @@
-import { createContext } from 'react';
+import React, { createContext, ReactNode } from 'react';
 import { User } from '../constants';
 
-const UserContext = createContext<User>({
-  name: 'default name',
+const defaultUser = {
+  name: 'Default Name',
   interests: [],
-});
+};
+export const UserContext = createContext<User>(defaultUser);
 
-export default UserContext;
+export const UserProvider = ({
+  children,
+}: {
+  children?: ReactNode;
+}): JSX.Element => {
+  return (
+    <UserContext.Provider value={defaultUser}>{children}</UserContext.Provider>
+  );
+};
