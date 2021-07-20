64ed8f4b8741a0b9488c5ab6a3f2084a01959337
Shih-Hao Chen <samuelshchen@gmail.com>
2021-07-20 12:06:54 +1200

    integrated images with user profile

---
+++ frontend/src/utils/dummyInterests.ts
@@ -0,0 +1,29 @@
+import { Interest } from '../pages/MapPage';
+
+export const dummyInterests = [
+  {
+    id: 1,
+    name: 'basketball',
+    emoji: '🏀',
+  } as Interest,
+  {
+    id: 2,
+    name: 'swimming',
+    emoji: '🏊',
+  } as Interest,
+  {
+    id: 3,
+    name: 'karate',
+    emoji: '🥋',
+  } as Interest,
+  {
+    id: 4,
+    name: 'cooking',
+    emoji: '🍳',
+  } as Interest,
+  {
+    id: 5,
+    name: 'soccer',
+    emoji: '⚽️',
+  } as Interest,
+];
--- frontend/src/utils/dummyUsers.ts
+++ frontend/src/utils/dummyUsers.ts
@@ -1,4 +1,7 @@
-import { Interest, UserProps } from '../pages/MapPage';
+import { UserProps } from '../pages/MapPage';
+import { dummyInterests } from './dummyInterests';
+import image1 from '../user-profiles/png/018-boy-3.png';
+import image2 from '../user-profiles/png/019-woman-5.png';
 
 export const useDummyUsers = (): UserProps[] => {
   const dummyUsers = [
@@ -9,54 +12,23 @@ export const useDummyUsers = (): UserProps[] => {
       lng: 174.7622,
       name: 'john stockman',
       description: 'love long walks on the beach',
-      interests: [
-        {
-          id: 1,
-          name: 'basketball',
-          emoji: '🏀',
-        } as Interest,
-        {
-          id: 2,
-          name: 'swimming',
-          emoji: '🏊',
-        } as Interest,
-        {
-          id: 3,
-          name: 'karate',
-          emoji: '🥋',
-        } as Interest,
-        {
-          id: 4,
-          name: 'cooking',
-          emoji: '🍳',
-        } as Interest,
-        {
-          id: 5,
-          name: 'soccer',
-          emoji: '⚽️',
-        } as Interest,
-      ],
-    } as UserProps,
+      image: image1,
+      interests: [0, 1, 2, 3, 4].map((interestIndex) => {
+        return dummyInterests[interestIndex];
+      }),
+    },
     {
       id: 2,
       // flat
       lat: -36.848869,
       lng: 174.781547,
       name: 'michaelangelo',
+      image: image2,
       description: 'slinging my numchucks',
-      interests: [
-        {
-          id: 2,
-          name: 'swimming',
-          emoji: '🏊',
-        } as Interest,
-        {
-          id: 3,
-          name: 'karate',
-          emoji: '🥋',
-        } as Interest,
-      ],
-    } as UserProps,
+      interests: [0, 3, 4].map((interestIndex) => {
+        return dummyInterests[interestIndex];
+      }),
+    },
   ];
 
   return dummyUsers;
