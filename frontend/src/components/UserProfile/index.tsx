4f9f7fc2e6205b63e23250d11e5a82c411ba3242
Shih-Hao Chen <samuelshchen@gmail.com>
2021-07-23 07:44:36 +1200

    refactored user profile

--- frontend/src/components/UserProfile/index.tsx
+++ frontend/src/components/UserProfile/index.tsx
@@ -1,114 +1,79 @@
 import { Avatar, Chip, Divider, Modal } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { UserProps } from '../../utils/types';
 import * as Styled from './UserProfile.styled';
 
 type UserProfileProps = {
-  user: UserProps;
-  isProfileOpen: boolean;
-  setCurrentUser: (user: UserProps) => void;
-  handleUpdateFriend: (index: number) => void;
+  user: UserProps | null;
+  onClose: () => void;
 };
 
 export const UserProfile = (props: UserProfileProps): JSX.Element => {
-  const { user } = props;
-  const {
-    index,
-    firstName,
-    lastName,
-    description,
-    interests,
-    isFriendsWithUser,
-  } = user;
-  const { isProfileOpen, setCurrentUser, handleUpdateFriend } = props;
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { user, onClose } = props;
 
   const handleClose = () => {
-    setCurrentUser({} as UserProps);
+    onClose();
   };
 
   const handleClick = () => {
     console.log('interest clicked');
   };
 
-  useEffect(() => {
-    if (!user) return;
-    setIsLoading(false);
-  }, [user]);
-
-  if (isLoading) {
-    return (
-      <div data-testid={'loading'}>
-        <span>loading...</span>
-      </div>
-    );
-  }
-
   return (
     <Modal
-      open={isProfileOpen}
+      open={user !== null}
       disableAutoFocus
       disableEnforceFocus
       onClose={handleClose}
       data-testid={'user-profile'}
     >
-      <Styled.ProfileDetailsContainer data-testid={'profile-details-container'}>
-        <Styled.UserHeader data-testid={'user-header'}>
-          <Styled.UserAvatar
-            alt={firstName}
-            src={user.image}
-            data-testid={'user-avatar'}
-          />
-          <Styled.UserName
-            data-testid={'user-name'}
-          >{`${firstName} ${lastName}`}</Styled.UserName>
-        </Styled.UserHeader>
-        <Divider />
-        <Styled.UserText variant="h4" data-testid={'bio-user-text'}>
-          Bio
-        </Styled.UserText>
-        <Styled.UserDescription data-testid={'user-description'}>
-          {description}
-        </Styled.UserDescription>
-        <Divider />
-        <Styled.UserText variant="h4" data-testid={'interests-user-text'}>
-          Interests
-        </Styled.UserText>
-        <Styled.UserInterests data-testid={'user-interests'}>
-          {interests &&
-            interests.map((interest) => (
-              <Chip
-                avatar={<Avatar>{interest.emoji}</Avatar>}
-                key={interest.id}
-                label={interest.name}
-                variant="outlined"
-                style={{ textTransform: 'capitalize' }}
-                onClick={handleClick}
-                data-testid={'interest-chip'}
-              />
-            ))}
-        </Styled.UserInterests>
-        <Styled.UserText>
-          Are you friends?: {isFriendsWithUser ? '👍' : '👎'}
-        </Styled.UserText>
-        {!isFriendsWithUser ? (
-          <Styled.UserAddFriendContainer>
-            <Styled.UserAddFriendButton
-              onClick={() => handleUpdateFriend(index)}
-            >
-              Add Friend
-            </Styled.UserAddFriendButton>
-          </Styled.UserAddFriendContainer>
-        ) : (
-          <Styled.UserAddFriendContainer>
-            <Styled.UserAddFriendButton
-              onClick={() => handleUpdateFriend(index)}
-            >
-              Remove Friend
-            </Styled.UserAddFriendButton>
-          </Styled.UserAddFriendContainer>
-        )}
-      </Styled.ProfileDetailsContainer>
+      {user ? (
+        <Styled.ProfileDetailsContainer
+          data-testid={'profile-details-container'}
+        >
+          <Styled.UserHeader data-testid={'user-header'}>
+            <Styled.UserAvatar
+              alt={user.firstName}
+              src={user.image}
+              data-testid={'user-avatar'}
+            />
+            <Styled.UserName data-testid={'user-name'}>
+              {user.firstName}
+            </Styled.UserName>
+          </Styled.UserHeader>
+          <Divider />
+          <Styled.UserText variant="h4" data-testid={'bio-user-text'}>
+            Bio
+          </Styled.UserText>
+          <Styled.UserDescription data-testid={'user-description'}>
+            {user.description}
+          </Styled.UserDescription>
+          <Divider />
+          <Styled.UserText variant="h4" data-testid={'interests-user-text'}>
+            Interests
+          </Styled.UserText>
+          <Styled.UserInterests data-testid={'user-interests'}>
+            {user.interests &&
+              user.interests.map((interest) => (
+                <Chip
+                  avatar={<Avatar>{interest.emoji}</Avatar>}
+                  key={interest.id}
+                  label={interest.name}
+                  variant="outlined"
+                  style={{ textTransform: 'capitalize' }}
+                  onClick={handleClick}
+                  data-testid={'interest-chip'}
+                />
+              ))}
+          </Styled.UserInterests>
+          <Styled.UserText>
+            Are you friends?: {user.isFriendsWithUser ? '👍' : '👎'}
+          </Styled.UserText>
+        </Styled.ProfileDetailsContainer>
+      ) : (
+        // If no user, return empty. This won't be seen anyway
+        <></>
+      )}
     </Modal>
   );
 };
