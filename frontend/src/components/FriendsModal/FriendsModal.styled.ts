b3f4887f15a3cd48e5298c15f5a36ecde3d477b7
Shih-Hao Chen <samuelshchen@gmail.com>
2021-08-11 09:03:56 +1200

    styled FriendsModal

--- frontend/src/components/FriendsModal/FriendsModal.styled.ts
+++ frontend/src/components/FriendsModal/FriendsModal.styled.ts
@@ -1,5 +1,4 @@
-import { Avatar, Paper, Tab, Tabs, Typography } from '@material-ui/core';
-import FilterListIcon from '@material-ui/icons/FilterList';
+import { Button, Fab, Paper, Tab, Tabs } from '@material-ui/core';
 import styled from 'styled-components';
 import { colours } from '../../utils/colours';
 
@@ -7,86 +6,60 @@ export const FriendsPaper = styled(Paper)`
   &&& {
     width: 30rem;
     max-width: 90%;
-    padding: 1.5rem;
+    border-radius: 0.3rem;
 
     top: 50%;
     left: 50%;
     position: absolute;
     transform: translate(-50%, -50%);
+
+    background-color: ${colours.paperBackground};
   }
 `;
 
 export const FriendsModalContainer = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 1.5rem;
+  overflow-y: scroll;
+  height: 60vh;
 `;
-
-export const FriendsModalHeader = styled.div`
-  width: 100%;
-`;
-
-export const FriendsModalHeadingText = styled(Typography)``;
 
 export const FriendsModalAppBar = styled.div`
+  border-radius: 0.3rem 0.3rem 0 0;
+
   width: 100%;
+  background-color: ${colours.primary};
 `;
 
 export const FriendsModalTabs = styled(Tabs)`
+  color: white;
   .PrivateTabIndicator-colorSecondary-3 {
     background-color: ${colours.secondary};
   }
 `;
 
-export const FriendsModalTab = styled(Tab)``;
-
-export const FriendsModalFilterIconContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
+export const FriendsModalTab = styled(Tab)`
+  width: 50%;
 `;
 
-export const TuneIcon = styled(FilterListIcon)`
-  float: right;
-  border-radius: 50%;
-  padding: 1%;
-  :hover {
-    background-color: ${colours.secondary};
+export const FriendsTuneFab = styled(Fab)`
+  &&& {
+    position: absolute;
+    bottom: 0.5rem;
+    right: 0.5rem;
+    background-color: ${colours.primary};
+    color: white;
+    opacity: 1;
+    z-index: 2;
   }
 `;
 
-export const FriendsModalUsersContainer = styled.div`
-  justify-content: center;
-  overflow-y: scroll;
-  height: 60vh;
-`;
-
-export const FriendsModalUserPill = styled.div`
-  width: 80%;
-  height: 4rem;
-  border-radius: 4rem;
-  padding: 0.1rem 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border: 0.05rem solid black;
-  box-shadow: 0.1rem 0.1rem ${colours.shadow};
-  margin-bottom: 1rem;
-  cursor: pointer;
-
-  :hover {
-    background-color: ${colours.shadow};
+export const FriendPillButton = styled(Button)`
+  &&& {
+    border: 1px solid grey;
+    background-color: #dddddd;
+    text-transform: none;
+    margin-top: 1%;
   }
 `;
-
-export const PillAvatar = styled(Avatar)`
-  max-width: 33%;
-`;
-
-export const PillText = styled(Typography)`
-  /* max-width: 33%; */
-`;
-
-export const PillEmojis = styled(Typography)`
-  max-width: 33%;
-`;
-
-export const FriendsContainer = styled.div``;
