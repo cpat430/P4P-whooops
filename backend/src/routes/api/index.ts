d3f5cb232dc7eb489970e602c5adcc259e344dfd
cpat430 <cpat430@aucklanduni.ac.nz>
2021-08-21 16:50:53 +1200

    added backend functionality for getting feedback

--- backend/src/routes/api/index.ts
+++ backend/src/routes/api/index.ts
@@ -1,20 +1,35 @@
 import { Router } from 'express';
 import { UserModel } from '../../db/user';
 import { v4 as uuid } from 'uuid';
+import { FeedbackModel } from '../../db/feedback';
 
 const router = Router();
 
 router.post('/user', (request, response) => {
   const { firstName, lastName, email } = request.body;
-  const id = uuid() as string;
+  const id = uuid();
 
   UserModel.create({ id, firstName, lastName, email })
     .then(() => {
-      console.log('✅ Successfully logged event');
+      console.log(`✅ Successfully created user ${id}`);
       response.status(200).send(id);
     })
     .catch(() => {
       console.log('🛑 Did not track event successfully');
+      response.status(400);
+    });
+});
+
+router.post('/feedback', (request, response) => {
+  const { id, email, question, answer, rating } = request.body;
+
+  FeedbackModel.create({ id, email, question, answer, rating })
+    .then(() => {
+      console.log(`✅ Successfully created feedback for ${email}`);
+      response.status(200).send(id);
+    })
+    .catch(() => {
+      console.log(`🛑 Unsuccessfully stored the feedback for ${email}`);
       response.status(400);
     });
 });
