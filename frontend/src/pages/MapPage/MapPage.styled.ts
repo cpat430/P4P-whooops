3f3559793580ea00987637987cdb36064ee571f5
Shih-Hao Chen <samuelshchen@gmail.com>
2021-08-24 14:48:56 +1200

    made challenge button pulse

--- frontend/src/pages/MapPage/MapPage.styled.ts
+++ frontend/src/pages/MapPage/MapPage.styled.ts
@@ -1,6 +1,5 @@
 import { Fab } from '@material-ui/core';
 import styled from 'styled-components';
-import { ChallengeButton } from '../../components/ChallengeButton';
 import { colours } from '../../utils/colours';
 
 export const MapDiv = styled.div`
@@ -22,16 +21,6 @@ export const FriendsFab = styled(Fab)`
   &&& {
     position: absolute;
     top: 6rem;
-    right: 1rem;
-    background-color: ${colours.primary};
-    color: white;
-  }
-`;
-
-export const StyledChallengeButton = styled(ChallengeButton)`
-  &&& {
-    position: absolute;
-    top: 11rem;
     right: 1rem;
     background-color: ${colours.primary};
     color: white;
