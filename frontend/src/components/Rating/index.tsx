cee98c1cb12c5158d30b404058125462724c2ed2
Shih-Hao Chen <samuelshchen@gmail.com>
2021-08-26 14:03:13 +1200

    slightly modified rating

--- frontend/src/components/Rating/index.tsx
+++ frontend/src/components/Rating/index.tsx
@@ -1,66 +1,38 @@
+import { Grid, Tooltip } from '@material-ui/core';
 import React from 'react';
-import { RatingContainer, RatingIconButton } from './Rating.styled';
-import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
-import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
-import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
-import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
-import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
-import { Tooltip } from '@material-ui/core';
+import { RatingIconButton } from './Rating.styled';
 
 type RatingProps = {
-  rating: number;
-  setRating: (rating: number) => void;
+  value: number | null;
+  allRatings: {
+    value: number;
+    text: string;
+    icon: JSX.Element;
+  }[];
+  onChange: (rating: number) => void;
 };
 
-export const Rating = (props: RatingProps): JSX.Element => {
-  const { rating, setRating } = props;
-
-  const handleClick = (value: number) => {
-    setRating(value);
-  };
-
+export const Rating = ({
+  value,
+  allRatings,
+  onChange,
+}: RatingProps): JSX.Element => {
   return (
-    <RatingContainer>
-      <Tooltip title="Very Dissatisfied">
-        <RatingIconButton
-          $isSelected={rating === 1}
-          onClick={() => handleClick(1)}
-        >
-          <SentimentVeryDissatisfiedIcon />
-        </RatingIconButton>
-      </Tooltip>
-      <Tooltip title="Dissatisfied">
-        <RatingIconButton
-          $isSelected={rating === 2}
-          onClick={() => handleClick(2)}
-        >
-          <SentimentDissatisfiedIcon />
-        </RatingIconButton>
-      </Tooltip>
-      <Tooltip title="Okay">
-        <RatingIconButton
-          $isSelected={rating === 3}
-          onClick={() => handleClick(3)}
-        >
-          <SentimentSatisfiedIcon />
-        </RatingIconButton>
-      </Tooltip>
-      <Tooltip title="Satisfied">
-        <RatingIconButton
-          $isSelected={rating === 4}
-          onClick={() => handleClick(4)}
-        >
-          <SentimentSatisfiedAltIcon />
-        </RatingIconButton>
-      </Tooltip>
-      <Tooltip title="Very Satisfied">
-        <RatingIconButton
-          $isSelected={rating === 5}
-          onClick={() => handleClick(5)}
-        >
-          <SentimentVerySatisfiedIcon />
-        </RatingIconButton>
-      </Tooltip>
-    </RatingContainer>
+    <Grid container justifyContent="center" alignItems="center" spacing={1}>
+      {allRatings.map((rating, i) => {
+        return (
+          <Grid item key={i}>
+            <Tooltip title={rating.text}>
+              <RatingIconButton
+                $isSelected={value === rating.value}
+                onClick={() => onChange(rating.value)}
+              >
+                {rating.icon}
+              </RatingIconButton>
+            </Tooltip>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
