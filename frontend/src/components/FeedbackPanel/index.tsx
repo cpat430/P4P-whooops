b60da667e3b85bac663eb4842f19034814c63eb3
Shih-Hao Chen <samuelshchen@gmail.com>
2021-08-28 11:06:35 +1200

    removed redundant div

--- frontend/src/components/FeedbackPanel/index.tsx
+++ frontend/src/components/FeedbackPanel/index.tsx
@@ -34,87 +34,84 @@ export const FeedbackPanel = ({
 
   return (
     <FeedbackPaper>
-      <div>
-        <div id="header"></div>
-        <Grid container justifyContent="center" alignItems="center" spacing={1}>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography>{question}</Typography>
-          </Grid>
-          <Grid item>
-            <Rating
-              value={ratingValue}
-              onChange={setRatingValue}
-              allRatings={[
-                {
-                  value: 1,
-                  text: 'Strongly Disagree',
-                  icon: <SentimentVeryDissatisfiedIcon />,
-                },
-                {
-                  value: 2,
-                  text: 'Disagree',
-                  icon: <SentimentDissatisfiedIcon />,
-                },
-                {
-                  value: 3,
-                  text: 'Neutral',
-                  icon: <SentimentSatisfiedIcon />,
-                },
-                {
-                  value: 4,
-
-                  text: 'Agree',
-                  icon: <SentimentSatisfiedAltIcon />,
-                },
-                {
-                  value: 5,
-                  text: 'Strongly Agree',
-                  icon: <SentimentVerySatisfiedIcon />,
-                },
-              ]}
-            ></Rating>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography>{'Further comments?'}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <FeedbackAnswerField
-              label="Your answer"
-              multiline
-              value={answer}
-              variant="outlined"
-              onChange={handleAnswerChange}
-            ></FeedbackAnswerField>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <SubmitButton
-              variant="contained"
-              disabled={ratingValue === null}
-              onClick={() => {
-                if (ratingValue === null) return;
-
-                addAppEvent({ name: 'submit-survey' });
-                onSubmit(question, ratingValue, answer);
-              }}
-              fullWidth
-            >
-              Submit
-            </SubmitButton>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
+      <Grid container justifyContent="center" alignItems="center" spacing={1}>
+        <Grid item xs={12}>
+          <Divider />
         </Grid>
-      </div>
+        <Grid item xs={12}>
+          <Typography>{question}</Typography>
+        </Grid>
+        <Grid item>
+          <Rating
+            value={ratingValue}
+            onChange={setRatingValue}
+            allRatings={[
+              {
+                value: 1,
+                text: 'Strongly Disagree',
+                icon: <SentimentVeryDissatisfiedIcon />,
+              },
+              {
+                value: 2,
+                text: 'Disagree',
+                icon: <SentimentDissatisfiedIcon />,
+              },
+              {
+                value: 3,
+                text: 'Neutral',
+                icon: <SentimentSatisfiedIcon />,
+              },
+              {
+                value: 4,
+
+                text: 'Agree',
+                icon: <SentimentSatisfiedAltIcon />,
+              },
+              {
+                value: 5,
+                text: 'Strongly Agree',
+                icon: <SentimentVerySatisfiedIcon />,
+              },
+            ]}
+          ></Rating>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>{'Further comments?'}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <FeedbackAnswerField
+            label="Your answer"
+            multiline
+            value={answer}
+            variant="outlined"
+            onChange={handleAnswerChange}
+          ></FeedbackAnswerField>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+        <Grid item xs={12}>
+          <SubmitButton
+            variant="contained"
+            disabled={ratingValue === null}
+            onClick={() => {
+              if (ratingValue === null) return;
+
+              addAppEvent({ name: 'submit-survey' });
+              onSubmit(question, ratingValue, answer);
+            }}
+            fullWidth
+          >
+            Submit
+          </SubmitButton>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+      </Grid>
     </FeedbackPaper>
   );
 };
