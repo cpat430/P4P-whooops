b5ad12a1e70a573e0e4e08568ac11f1950b4bedf
cpat430 <cpat430@aucklanduni.ac.nz>
2021-09-11 12:36:48 +1200

    added the city countdowns

--- frontend/src/utils/locations.ts
+++ frontend/src/utils/locations.ts
@@ -17,3 +17,18 @@ export const albertParkPos = {
   lat: -36.850677,
   lng: 174.767598,
 };
+
+export const quayStreetCountdown = {
+  lat: -36.84460279365626,
+  lng: 174.77308801116143,
+};
+
+export const vicStreetCountdown = {
+  lat: -36.848586830337474,
+  lng: 174.76471164628379,
+};
+
+export const albertStreetCountdown = {
+  lat: -36.8431351455885,
+  lng: 174.7653208149553,
+};
