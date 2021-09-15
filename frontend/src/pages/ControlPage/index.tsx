616f3390887653026516d46c232ab7e2f320b2de
Shih-Hao Chen <samuelshchen@gmail.com>
2021-09-16 10:07:54 +1200

    make control panel look nicer

--- frontend/src/pages/ControlPage/index.tsx
+++ frontend/src/pages/ControlPage/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Button, Card, Grid, Typography } from '@material-ui/core';
 import React, { useContext } from 'react';
 import {
   allEnvironments,
@@ -15,48 +15,74 @@ export const ControlPage = (): JSX.Element => {
       container
       justifyContent="center"
       alignItems="center"
-      style={{ height: '100vh', width: '100vw', backgroundColor: '#eee' }}
+      style={{ height: '100vh', width: '100vw', backgroundColor: '#e5e5e5' }}
     >
       <Grid item>
-        <Grid container justifyContent="center" alignItems="center" spacing={1}>
-          <Grid item xs={12}>
-            <Typography variant="h6">Control Panel</Typography>
+        <Card style={{ padding: '2rem' }}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            style={{ maxWidth: '50vw' }}
+          >
+            <Grid item xs={12}>
+              <Typography variant="h5">Control Panel</Typography>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  Environments
+                </Grid>
+                {allEnvironments.map((environment: Environment, i: number) => {
+                  return (
+                    <Grid item key={i}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                          io.emit('change-env', environment);
+                        }}
+                      >
+                        {environment.name}
+                      </Button>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  Change Testing Group
+                </Grid>
+                {(
+                  [
+                    'no-interest-badge',
+                    'similar-interests',
+                    'all-interests',
+                  ] as const
+                ).map((testingGroup: TestingGroup, i: number) => {
+                  return (
+                    <Grid item key={i}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                          io.emit('change-testing-group', testingGroup);
+                        }}
+                      >
+                        {testingGroup.replaceAll('-', ' ')}
+                      </Button>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Grid>
           </Grid>
-
-          {allEnvironments.map((environment: Environment, i: number) => {
-            return (
-              <Grid item key={i}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => {
-                    io.emit('change-env', environment);
-                  }}
-                >
-                  {environment.name}
-                </Button>
-              </Grid>
-            );
-          })}
-
-          {(
-            ['no-interest-badge', 'similar-interests', 'all-interests'] as const
-          ).map((testingGroup: TestingGroup, i: number) => {
-            return (
-              <Grid item key={i}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => {
-                    io.emit('change-testing-group', testingGroup);
-                  }}
-                >
-                  Testing Group {testingGroup}
-                </Button>
-              </Grid>
-            );
-          })}
-        </Grid>
+        </Card>
       </Grid>
     </Grid>
   );
